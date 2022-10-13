7decf9790e5c5a861901dd842dc6301af3f40936
karimzaggoti <karimzaggoti@localhost>
2022-10-13 11:58:37 -0500

    pusshed

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -29,44 +29,42 @@ public class InterpretDrawingFile {
 		boolean filled = in.nextBoolean();
 
 		if (shapeType.equals("rectangle")) {
-            double x = in.nextDouble();
-            double y = in.nextDouble();
-            double halfWidth = in.nextDouble();
-            double halfHeight = in.nextDouble();
-            if (filled) {
-            	StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
-            }
-            else if (!filled) {
-            	StdDraw.rectangle(x, y, halfWidth, halfHeight);
-            } 
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			if (filled) {
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			} else if (!filled) {
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
 		}
 		if (shapeType.equals("triangle")) {
-			 double x1 = in.nextDouble();
-	         double y1 = in.nextDouble();
-	         double x2 = in.nextDouble();
-	         double y2 = in.nextDouble();
-	         double x3 = in.nextDouble();
-	         double y3 = in.nextDouble();
-	         double [] xs = {x1, x2, x3};
-	         double [] ys = {y1, y2, y3};
-	         if (filled) {
-	        	StdDraw.filledPolygon(xs, ys);
-	         }
-	         if (!filled) {
-	        	 StdDraw.polygon(xs, ys);
-	         }
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			double[] xs = { x1, x2, x3 };
+			double[] ys = { y1, y2, y3 };
+			if (filled) {
+				StdDraw.filledPolygon(xs, ys);
+			}
+			if (!filled) {
+				StdDraw.polygon(xs, ys);
+			}
 		}
 		if (shapeType.equals("ellipse")) {
 			double x = in.nextDouble();
-            double y = in.nextDouble();
-            double halfWidth = in.nextDouble();
-            double halfHeight = in.nextDouble();
-            if (filled) {
-            	StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
-            }
-            else if (!filled) {
-            	StdDraw.ellipse(x, y, halfWidth, halfHeight);
-            } 
+			double y = in.nextDouble();
+			double semiMajorAxis = in.nextDouble();
+			double semiMinorAxis = in.nextDouble();
+			if (filled) {
+				StdDraw.filledEllipse(x, y, semiMajorAxis, semiMinorAxis);
+			} else if (!filled) {
+				StdDraw.ellipse(x, y, semiMajorAxis, semiMinorAxis);
+			}
 		}
 	}
 }
